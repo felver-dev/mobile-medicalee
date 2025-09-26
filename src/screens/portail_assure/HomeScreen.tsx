767b5ae6b4022408e7c3449859adf2aa6d60b18f
felver-dev <felver.ngouala@gmail.com>
2025-09-26 11:11:16 +0000

    fixed the border in the home screen cart

--- src/screens/portail_assure/HomeScreen.tsx
+++ src/screens/portail_assure/HomeScreen.tsx
@@ -454,14 +454,8 @@ const styles = StyleSheet.create({
     borderRadius: 16,
     padding: 16,
     marginBottom: 12,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.1,
-    shadowRadius: 8,
-    elevation: 3,
+    borderWidth: 1,
+    borderColor: '#E5E7EB',
   },
   actionIcon: {
     width: 48,
